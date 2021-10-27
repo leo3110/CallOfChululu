a34eefc851ff3d209f5a060368edb5c994bcb903
Leo <leo3110.souza@gmail.com>
2021-10-27 18:56:45 -0300

    just filling the card

--- Object/ficha.php
+++ Object/ficha.php
@@ -1,4 +1,9 @@
 <?php
-  $ficha = [nome,jogador,ocupacao,idade,sexo,residencia,nascimento,for,des,pod,con,apa,edu,tam,int,tdm,lg,it,ii,
-  sanidade,pdv,sorte,pdm,];
+const nomeDeVariaveis = ["nome","jogador","ocupacao","idade","sexo","residencia","nascimento
+força","destreza","poder","conhecimento","aparencia","educação ou conhecimento","tamanho","inteligencia","taxa de movimnto","lesão grave","insanidade temporaria","insanidade indefinida","
+sanidade","pontos de vida","sorte","pontos de magia","perícias do investigador];
+
+  $ficha = [nome","jogador","ocupacao","idade","sexo","residencia","nascimento","for","des","pod","con","apa","edu","tam","int","tdm","lg","it","ii","
+  sanidade","pdv","sorte","pdm"];
+
  ?>
